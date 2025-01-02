4039608ebb6fff14d0dd72bb0b0676a7f1592aaf
KwonSiyoon <tldbsrnjs@naver.com>
2025-01-02 17:50:45 +0900

    조금 수정

--- Cpp/CppString/BankAccount/Main.cpp
+++ Cpp/CppString/BankAccount/Main.cpp
@@ -3,8 +3,8 @@
 class Account
 {
 public:
-	Account(int id, const char* name, int balance)
-		: id(id), balance(balance)
+	Account(int id, const char* name, int money)
+		: id(id), balance(money)
 	{
 		size_t length = strlen(name) + 1;
 		this->name = new char[length];
@@ -35,9 +35,9 @@ public:
 			delete donationAccount;
 		}
 	}
-	virtual void Deposit(float balance)
+	virtual void Deposit(float money)
 	{
-		if (balance <= 0)
+		if (money <= 0)
 		{
 			std::cout << "0이상의 숫자를 입력하세요.\n";
 			int num;
@@ -46,12 +46,12 @@ public:
 		}
 		else
 		{
-			this->balance += balance;
+			this->balance += money;
 		}
 	}
-	virtual void Withdraw(int balance)
+	virtual void Withdraw(int money)
 	{
-		if (balance <= 0)
+		if (money <= 0)
 		{
 			std::cout << "0이상의 숫자를 입력하세요.\n";
 			int num;
@@ -60,13 +60,13 @@ public:
 		}
 		else
 		{
-			if (balance > this->balance)
+			if (money > this->balance)
 			{
 				std::cout << "잔액이 부족합니다.\n";
 			}
 			else
 			{
-				this->balance -= balance;
+				this->balance -= money;
 				std::cout << "잔액 : " << this->balance << "\n";
 			}
 		}
@@ -95,11 +95,14 @@ private:
 class CreditAccount : public Account
 {
 public:
-	virtual void Deposit(float balance) override
+	CreditAccount();
+	~CreditAccount();
+	
+	virtual void Deposit(float money) override
 	{
-		interest += balance * interestRate;
+		interest += money * interestRate;
 	}
-	virtual void Withdraw(int balance) override
+	virtual void Withdraw(int money) override
 	{
 
 	}
@@ -115,11 +118,13 @@ private:
 class DonationAccount : public Account
 {
 public:
-	virtual void Deposit(float balance) override
+	DonationAccount();
+	~DonationAccount();
+	virtual void Deposit(float money) override
 	{
-		donation += balance * donationRate;
+		donation += money * donationRate;
 	}
-	virtual void Withdraw(int balance) override
+	virtual void Withdraw(int money) override
 	{
 
 	}
@@ -145,31 +150,31 @@ public:
 			}
 		}
 	}
-	void CreateAccount(const char* name, float balance)
+	void CreateAccount(const char* name, float money)
 	{
-		accounts[lastNum] = new Account(lastNum, name, balance);
+		accounts[lastNum] = new Account(lastNum, name, money);
 		accounts[lastNum]->Print();
 		std::cout << "계좌가 개설되었습니다.\n";
 		++lastNum;
 	}
-	void Deposit(int id, float balance)
+	void Deposit(int id, float money)
 	{
 		if (accounts[id] == nullptr)
 		{
 			std::cout << "없는 계좌번호입니다.\n";
 			return;
 		}
-		accounts[id]->Deposit(balance);
+		accounts[id]->Deposit(money);
 		accounts[id]->Print();
 	}
-	void Withdraw(int id, float balance)
+	void Withdraw(int id, float money)
 	{
 		if (accounts[id] == nullptr)
 		{
 			std::cout << "없는 계좌번호입니다.\n";
 			return;
 		}
-		accounts[id]->Withdraw(balance);
+		accounts[id]->Withdraw(money);
 		accounts[id]->Print();
 	}
 	void Inquire()
@@ -193,7 +198,7 @@ void Loop()
 	char input;
 	char* name{};
 	int id;
-	int balance;
+	int money;
 	while (true)
 	{
 		std::cout << "1.계좌 개설  2.입금  3.출금  4.전체 고객 잔액 조회\n";
@@ -207,24 +212,24 @@ void Loop()
 			std::cout << "이름을 입력해주세요.\n";
 			std::cin >> name;
 			std::cout << "입금할 금액을 입력해주세요.\n";
-			std::cin >> balance;
-			bank.CreateAccount(name, balance);
+			std::cin >> money;
+			bank.CreateAccount(name, money);
 		}
 		else if (input == '2')			// 입금
 		{
 			std::cout << "계좌번호를 입력해주세요.\n";
 			std::cin >> id;
 			std::cout << "입금할 금액을 입력해주세요.\n";
-			std::cin >> balance;
-			bank.Deposit(id, balance);
+			std::cin >> money;
+			bank.Deposit(id, money);
 		}
 		else if (input == '3')			// 출금
 		{
 			std::cout << "계좌번호를 입력해주세요.\n";
 			std::cin >> id;
 			std::cout << "출금할 금액을 입력해주세요.\n";
-			std::cin >> balance;
-			bank.Withdraw(id, balance);
+			std::cin >> money;
+			bank.Withdraw(id, money);
 		}
 		else if (input == '4')			// 전체 고객 잔액 조회
 		{
