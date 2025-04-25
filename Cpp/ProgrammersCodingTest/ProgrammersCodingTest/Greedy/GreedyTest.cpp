a01ded2ea8eb86a7ff5117a9678edbeccc08cd48
KwonSiyoon <tldbsrnjs@naver.com>
2025-04-25 13:58:13 +0900

    Solve Greedy Joystick

--- Cpp/ProgrammersCodingTest/ProgrammersCodingTest/Greedy/GreedyTest.cpp
+++ Cpp/ProgrammersCodingTest/ProgrammersCodingTest/Greedy/GreedyTest.cpp
@@ -11,12 +11,33 @@ int GreedyTest::JoystickSolution(std::string name)
     // 0 1 2 3 4 5 6 7 8 9 0   1   2   3   4 5 6 7 8 9 0 1 2 3 4 5
     // 0 1 2 3 4 5 6 7 8 9 10  11  12  11 10 9 8 7 6 5 4 3 2 1
     // 아스키 65~90
-    int currentIndex = 0;
-    int size = name.size();
-    int* count = new int[size];
+    int length = name.length();
+    int answer = 0;
 
+    for (int i = 0; i < length; ++i)
+    {
+        answer += std::min(name[i] - 'A', 'Z' - name[i] + 1);
+    }
 
+    int minMove = length - 1;
 
-    delete[] count;
-    return 0;
+    for (int i = 0; i < length; ++i)
+    {
+        int next = i + 1;
+
+        while (next < length && name[next] == 'A')
+        {
+            ++next;
+        }
+
+        int move = i * 2 + (length - next);
+        int reverse = i + (length - next) * 2;
+
+        minMove = std::min({ minMove, move, reverse });
+
+    }
+
+    answer += minMove;
+
+    return answer;
 }
